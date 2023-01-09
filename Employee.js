81b48c92498137fc248d3b096b480a914ac573e6
Hallel <halleli100@gmail.com>
2023-01-09 14:40:45 +0200

    cards in a vartical

--- Employee.js
+++ Employee.js
@@ -13,11 +13,20 @@ function createEmployee() {
     createEmployeeCard(name, email)
 }
 
+function getRandomId() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    
+
+}
+
 function createEmployeeCard(name, email) {
     let all_employees = document.getElementById("allEmployees");
     let employee_div = document.createElement("div");
-
-    employee_div.id = 'employeeTagId';
+    let id = getRandomId()
+    console.log(id)
+    employee_div.id = 'employeeTagId_' + id;
     employee_div.style.width = "100px";
     employee_div.style.padding = "0px 10px 20px 30px";
     employee_div.style.border = "2px solid black";
