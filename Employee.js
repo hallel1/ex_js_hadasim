f4e7325231d2e3f6175a89de5e07848eea4df096
Hallel <halleli100@gmail.com>
2023-01-09 13:20:33 +0200

    split file

--- Employee.js
+++ Employee.js
@@ -1,37 +1,11 @@
 import {NAME, EMAIL} from "./consts.js";
+import {createForm} from "./CreateForm.js";
 
 window.onload = (event) => {
     createForm()
     const submit = document.getElementById("submitButton");
     submit.addEventListener('click', createEmployee);
-
 };
-
-function createForm() {
-    let form = document.getElementById("form_fields")
-    createFormField(form, NAME, "name:", "text")
-    createFormField(form, EMAIL, "email:","text")
-}
-
-function createFormField(form, field_name, field_label, inputType) {
-    form.appendChild(createFormFieldLabel(field_name, field_label))
-    form.appendChild(createFormFieldInput(field_name, inputType))
-}
-
-function createFormFieldLabel(field_name, field_label) {
-    let label = document.createElement("label");
-    label.htmlFor = field_name
-    label.innerHTML = field_label;
-    return label;
-}
-
-function createFormFieldInput(field_name, inputType) {
-    let input = document.createElement("input");
-    input.type = inputType
-    input.id = field_name
-    input.name = field_name
-    return input;
-}
 
 function createEmployee() {
     let name = document.getElementById(NAME).value;
